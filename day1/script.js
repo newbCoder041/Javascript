f73b98d0a62275712447e2f7ebaf609d7d8a78f3
KhiulAakash <khiulaakash123@gmail.com>
2024-08-07 19:52:21 +0545

    day 1

---
+++ day1/script.js
@@ -0,0 +1,69 @@
+// console.log("Hello")
+
+// let value=true;
+// console.log(value)
+// console.log(typeof value)
+// value=String(value) //Boolean is converted into string
+// console.log(typeof value)
+// console.log(value)
+
+// console.log('2'/'2') //String are converted to numbers
+
+// let str='123';
+// str=Number(str);
+// console.log(Number(str))
+// console.log(typeof str)
+
+// let age=Number('I love you');
+// console.log(age) //If the string is not a valid number, the result of such conversion is NAN.
+
+// console.log(Number('123'))
+// console.log(Number('123z'))
+// console.log(Number(true))
+// console.log(Number(false)) 
+
+// console.log(Boolean(1))
+// console.log(Boolean(0))
+// console.log(Boolean('Aakash'))
+// console.log(Boolean(''))
+// console.log(Boolean(' '))//non-empty strings as truthy values, even if they contain only whitespace
+
+// const num=0;
+// console.log(num??100)
+
+/*Data Types in Javascript
+1. Primitive Data Type
+    a. Number
+    b. String
+    c. Boolean
+    d. Null
+    e. Undefined
+    f. Bigint 
+    g. Symbol
+2. Non-primitive Data Type or Object Data Type
+    a. Object
+    b. Array
+    c. Function*/
+
+    // let num=1/0;
+    // console.log(num==Infinity)
+// console.log(isNaN('123z'))
+
+// let map=new Map();
+// map.set('1','str1');
+// map.set('2','str2')
+// console.log(map)
+// console.log(map.get('1'))
+// console.log(map.size)
+
+// let set=new Set()
+// set.add(1)
+// set.add(2)
+// set.add(3)
+// set.add(3)
+// console.log(set)
+// console.log(set.has(3))
+// set.delete(3)
+// console.log(set)
+// set.clear();
+// console.log(set)
